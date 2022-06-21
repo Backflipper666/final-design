eaf520e91590c19f5e3c46fcae0eb17af5116f4d
Backflipper666 <elderly619@gmail.com>
2022-06-22 04:47:07 +0600

    make sidebar appear on click on mobile and tablet

--- src/js/script1-4.js
+++ src/js/script1-4.js
@@ -2,9 +2,29 @@ let burger = document.querySelector('.upperMenu__burger')
 
 burger.addEventListener('mouseover', () => {
   burger.setAttribute('style', 'border: 2px solid #B8FFEC;')
-  console.log('works fine mate')
 })
 burger.addEventListener('mouseleave', () => {
   burger.setAttribute('style', 'border: none;')
-  console.log('works fine mate')
 })
+
+let block4 = document.querySelector('.wrapper1-4')
+
+let rest = document.querySelectorAll(
+  '.upperMenu, .about, .services, .description, .wrapper-header, .labels, .swiper, .btn'
+)
+let body = document.body
+let html = document.documentElement
+
+if ($(window).width() < 1199) {
+  burger.addEventListener('click', () => {
+    block4.setAttribute('style', 'visibility:visible; display:block;')
+    for (element of rest) {
+      element.setAttribute('style', 'visibility:hidden; display:none;')
+    }
+    body.setAttribute(
+      'style',
+      'display:flex; align-items:stretch; margin:0; height:100%;'
+    )
+    html.setAttribute('style', 'height:100%;')
+  })
+}
