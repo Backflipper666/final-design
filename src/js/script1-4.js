fa1d72c620f21210ce63be0f6a060a2480254377
Backflipper666 <elderly619@gmail.com>
2022-06-22 18:31:25 +0600

    fix the sidebar height bug

--- src/js/script1-4.js
+++ src/js/script1-4.js
@@ -12,19 +12,34 @@ let block4 = document.querySelector('.wrapper1-4')
 let rest = document.querySelectorAll(
   '.upperMenu, .about, .services, .description, .wrapper-header, .labels, .swiper, .btn'
 )
+
 let body = document.body
 let html = document.documentElement
 
 if ($(window).width() < 1199) {
   burger.addEventListener('click', () => {
-    block4.setAttribute('style', 'visibility:visible; display:block;')
-    for (element of rest) {
-      element.setAttribute('style', 'visibility:hidden; display:none;')
-    }
-    body.setAttribute(
+    block4.setAttribute(
       'style',
-      'display:flex; align-items:stretch; margin:0; height:100%;'
+      'visibility:visible; display:block; height:100vh;'
     )
-    html.setAttribute('style', 'height:100%;')
+    for (element of rest) {
+      element.setAttribute('style', 'visibility:hidden; ')
+    }
   })
 }
+
+let closeBtn = document.querySelector('.upperMenu1-4__burger')
+
+closeBtn.addEventListener('mouseover', () => {
+  closeBtn.setAttribute('style', 'border: 2px solid #B8FFEC;')
+})
+closeBtn.addEventListener('mouseleave', () => {
+  closeBtn.setAttribute('style', 'border: none;')
+})
+
+closeBtn.addEventListener('click', () => {
+  block4.setAttribute('style', 'visibility:hidden; display:none;')
+  for (element of rest) {
+    element.setAttribute('style', 'visibility:visible; ')
+  }
+})
