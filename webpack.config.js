004884c4007c9944afc41396117dab26f6288949
Backflipper666 <elderly619@gmail.com>
2022-06-29 07:56:02 +0600

    install Clean-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   // Входной файл
@@ -85,6 +86,7 @@ module.exports = {
         from: './src/img',
         to: 'img'
       }
-    ])
+    ]),
+    new CleanWebpackPlugin()
   ]
 }
